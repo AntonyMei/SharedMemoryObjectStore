78ac977f11984e0617f296c21626e6f0c98c1cc6
Antony <meiyixuan2000@126.com>
2021-11-24 23:17:06 +0800

    add general serializer and deserializer

--- function_tests.py
+++ function_tests.py
@@ -20,8 +20,8 @@ def main():
     # print(res)
     ec1 = object_store_util.EntryConfig(dtype=2123, shape=[np.ones(3), np.zeros(4), 1, "abc"])
     ec2 = object_store_util.EntryConfig(dtype=2123, shape=ec1)
-    config_list, stream_list = object_store_util.serialize_list([ec1, ec2])
-    res = object_store_util.deserialize_list(stream_list)
+    config_list, stream_list = object_store_util.serialize([ec1, ec2])
+    res = object_store_util.deserialize(stream_list)
     print(res)
 
     end = time.time()
