02e28b5455fb8a4cd8cd7b500509d1da546c9f24
Antony <meiyixuan2000@126.com>
2021-12-01 18:08:10 +0800

    add SMOS_server.py

---
+++ SMOS_server.py
@@ -0,0 +1,15 @@
+"""
+2021.12.01 Yixuan Mei
+This file contains class SMOSServer, which manages SharedMemoryObjectStore in a remote process.
+"""
+
+import SMOS_exceptions
+import SMOS_utils as utils
+from SMOS_constants import SMOS_FAIL, SMOS_SUCCESS
+from SMOS_shared_memory_object_store import SharedMemoryObjectStore
+
+
+class SMOSServer:
+
+    def __init__(self):
+        pass
