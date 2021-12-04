b1f89cebf09fde8f3afa23c0eae3408762f36a06
Antony <meiyixuan2000@126.com>
2021-12-04 23:22:09 +0800

    add SMOS.py, which contains everything that needs to be imported for SMOS

---
+++ SMOS.py
@@ -0,0 +1,12 @@
+"""
+2021.12.04 Yixuan Mei
+This file contains everything that needs to be imported for SMOS.
+"""
+
+import SMOS_exceptions
+import SMOS_utils
+
+from SMOS_constants import SMOS_SUCCESS, SMOS_FAIL, SMOS_PERMISSION_DENIED
+from SMOS_utils import ConnectionDescriptor
+from SMOS_server import Server
+from SMOS_client import Client
