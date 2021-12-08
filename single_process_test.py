c8dc12f6d51dd1a3ee5321309a26ea01c4693733
Antony <meiyixuan2000@126.com>
2021-12-08 16:21:06 +0800

    finished test for get

--- single_process_test.py
+++ single_process_test.py
@@ -158,9 +158,12 @@ def main():
     """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
     # 7: test get
-    data = [124, np.random.randn(10), SMOS_utils.EntryConfig(1, (1, 2), False)]
-    client.put(name="obj1", data=data)
-    print(client.get(name="obj1"))
+    # data = [124, np.random.randn(10), SMOS_utils.EntryConfig(1, (1, 2), False)]
+    # client.put(name="obj1", data=data)
+    # print(client.get(name="obj1"))
+    # print(client.get(name="obj1"))
+
+    """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
     # clean up
     print("test finished")
